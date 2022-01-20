c56012d1538aaaada09c124e03164c1e746c33ef
Jeanapril <95842409+Jeanapril@users.noreply.github.com>
2022-01-20 16:52:24 +0900

    Jieun Test

---
+++ GitTest/src/main/java/Bye.java
@@ -0,0 +1,15 @@
+
+public class Bye {
+
+	public static void main(String[] args) {
+		
+		System.out.println("스방신기");
+		System.out.println("최강동민");
+		System.out.println("믹키준선");
+		System.out.println("유노찬호");
+		System.out.println("시아성규");
+		System.out.println("영웅지은");
+
+	}
+
+}
