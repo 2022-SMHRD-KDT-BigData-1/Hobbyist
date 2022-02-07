4ff329963060adab1f95ccf37ef8997460f9fde3
smhrd <smhrd@220.80.203.113>
2022-02-07 12:41:24 +0900

    Community delete select upload Con 추가 - 차노

---
+++ Hobbyist/src/main/java/com/message/service/CommunitySelectCon.java
@@ -0,0 +1,40 @@
+package com.message.service;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.catalina.filters.SetCharacterEncodingFilter;
+
+import com.message.model.AcademyDTO;
+import com.message.model.CommunityDAO;
+import com.message.model.CommunityDTO;
+import com.message.model.MemberDAO;
+
+@WebServlet("/CommunityCon")
+public class CommunitySelectCon extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		request.setCharacterEncoding("UTF-8"); 
+		
+		CommunityDAO dao = new CommunityDAO();
+		
+		ArrayList<CommunityDTO> list=  dao.commSelect();
+		request.setAttribute("list", list);
+		
+		RequestDispatcher dispatcher=request.getRequestDispatcher("community.jsp");
+		dispatcher.forward(request, response);
+		
+		
+		
+	}
+
+}
