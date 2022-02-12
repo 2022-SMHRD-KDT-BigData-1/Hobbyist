9f3472f221434d23047ee478d54d2f1c8e920f60
smhrd <smhrd@211.48.228.6>
2022-02-12 17:08:39 +0900

    Recommend.jsp DAO CON - by 준선

--- Hobbyist/src/main/java/com/message/service/RecommendSelectCon.java
+++ Hobbyist/src/main/java/com/message/service/RecommendSelectCon.java
@@ -9,6 +9,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.message.model.AcademyDTO;
 import com.message.model.RecommendDAO;
@@ -19,9 +20,17 @@ public class RecommendSelectCon extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		
+		if(session.getAttribute("recommend") != null) {
+			session.removeAttribute("recommend");
+			session.removeAttribute("count");
+		}
+		
+		
 		request.setCharacterEncoding("utf-8");
-		String a_L_category = request.getParameter("a_L_category"); 
-		String a_m_category = request.getParameter("a_m_category"); 
+		String a_L_category = request.getParameter("a_L_category1"); 
+		String a_m_category = request.getParameter("a_m_category1"); 
 		String a_city = request.getParameter("city"); 
 		
 		AcademyDTO Recommend = new AcademyDTO(null,null,null,a_L_category, a_m_category, null, a_city);
@@ -29,10 +38,15 @@ public class RecommendSelectCon extends HttpServlet {
 		
 		ArrayList<AcademyDTO> recommend =  dao.recSelect(Recommend);
 		
-		request.setAttribute("recommend", recommend);
+		int count = 0;
+		count = dao.getCount(Recommend); 
+		System.out.println(count);
 		
-		RequestDispatcher dispatcher=request.getRequestDispatcher("Recommend.jsp"); 
-		dispatcher.forward(request, response);
+		session.setAttribute("recommend", recommend);
+		session.setAttribute("count", count);
+		
+		response.sendRedirect("Recommend.jsp");
+		
 		
 		
 		
