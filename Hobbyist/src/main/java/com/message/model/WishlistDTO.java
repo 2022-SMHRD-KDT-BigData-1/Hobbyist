193aad7bc49e597c83985bfa49854fb3b16faaa9
Jeanapril <95842409+Jeanapril@users.noreply.github.com>
2022-02-15 15:46:34 +0900

    메인 / 위시리스트 + 지도  by지은

---
+++ Hobbyist/src/main/java/com/message/model/WishlistDTO.java
@@ -0,0 +1,26 @@
+package com.message.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data //어노테이션
+//@Data의 역할은 getter/setter 메소드를 자동 생성
+
+@NoArgsConstructor
+//기본 생성자를 생성해줌.
+
+@AllArgsConstructor
+//모든 필드변수를 초기화하는 생성자를 생성해줌.
+//변수, 생성자, getter/setter, toString() 정의
+
+public class WishlistDTO {
+	private int w_seq;
+	private String m_email;
+	private String a_name;
+	private String a_city;
+	private String a_address;
+	private String w_tel;
+	
+
+}
